c839d61fb999a901c9dc7c93707e6ab1340c6207
LeoProk <just_beta@hotmail.com>
2018-03-16 23:39:46 +0200

    added json get

--- Podfile
+++ Podfile
@@ -1,12 +1,11 @@
 source 'https://github.com/CocoaPods/Specs.git'
 platform :ios, '10.0'
+initial commit
 
 target 'XRMwallet-swift' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
-  use_frameworks!
 
-  # Pods for XRMwallet-swift
  pod 'Alamofire', '~> 4.7' 
+
  target 'XRMwallet-swiftTests' do
     inherit! :search_paths
     # Pods for testing
---
+++ XRMwallet-swift/WalletRPC.swift
@@ -0,0 +1,42 @@
+//
+//  WalletRPC.swift
+//  XRMwallet-swift
+//
+//  Created by user on 3/16/18.
+//  Copyright © 2018 user. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class WalletRPC{
+    init(command:String) {
+        switch command {
+        case "balance":
+            getBalance()
+        default:
+            return
+        }
+    }
+    func sendMonero() {
+        
+    }
+    
+    
+    func getBalance() -> String {
+        let yay = Alamofire.request("http://mine.moneropool.com:3333/pool/blocks",method: .get).responseJSON { response in
+            print("Request: \(String(describing: response.request))")   // original url request
+            print("Response: \(String(describing: response.response?.statusCode))") // http url response
+            print("Result: \(response.result)")                         // response serialization result
+            
+            if let json = response.result.value {
+                print("JSON: \(json)") // serialized json response
+            }
+            
+            if let data = response.data, let utf8Text = String(data: data, encoding: .utf8) {
+                print("Data: \(utf8Text)") // original server data as UTF8 string
+            }
+        }
+        return ""
+    }
+}
